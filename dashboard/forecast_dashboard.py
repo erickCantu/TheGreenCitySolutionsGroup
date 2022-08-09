88aabfad676c4cbaec8eff745c66e2f93be1ab95
Rafael Arndt <r4f@HisComputer>
2022-08-09 09:34:37 +0200

    major dashboard update

--- dashboard/forecast_dashboard.py
+++ dashboard/forecast_dashboard.py
@@ -3,24 +3,38 @@ from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+import sys
+sys.path.append("../notebooks/")
+from green_city.utils import datetime2index, index2datetime
 
-dfs = {}
-for i in range(1, 10):
-    dfs[i] = (pd.read_csv(f"../data/preprocessed/Building_{i}.csv")
-        .astype({"datetime": "datetime64"})
-        .set_index("datetime")
-    )
-dfs['all'] = (pd.read_csv(f"../data/preprocessed/Agg_buildings.csv")
-    .astype({"datetime": "datetime64"})
-    .set_index("datetime")
-)
+#dfs = {}
+#for i in range(1, 10):
+#    dfs[i] = (pd.read_csv(f"../data/preprocessed/Building_{i}.csv")
+#        .astype({"datetime": "datetime64"})
+#        .set_index("datetime")
+#    )
+#dfs['all'] = (pd.read_csv(f"../data/preprocessed/Agg_buildings.csv")
+#    .astype({"datetime": "datetime64"})
+#    .set_index("datetime")
+#)
+#
+#forecasts = {}
+#for model in ["linreg_poly", "random_forest", "xgboost", "xgboost_reduced_features"]:
+#    forecasts[model] = {}
+#    for i in [*list(range(1, 10)), "all"]:
+#        forecasts[model][i] = pd.read_pickle(f'../data/models/{model}_building_{i}.pkl')
 
-forecasts = {}
-forecasts["all"] = pd.read_pickle('../data/models/linreg_poly_building_all.pkl')
-for i in range(1, 10):
-    forecasts[i] = pd.read_pickle(f'../data/models/linreg_poly_building_{i}.pkl')
+pred_indices = [32135, 33311, 26478, 33357, 30387, 30794, 31800, 28783]
+pred_times = [index2datetime(pi) for pi in pred_indices]
+pred_time_hours = [(d.strftime("%Y-%m-%d"), d.strftime("%H")) for d in pred_times]
+
+all_forecasts = pd.read_pickle("../data/models/all_models.pkl")
+
+set_date = "2011-08-09"
+set_time = "18"
 
 app = Dash(external_stylesheets=[dbc.themes.FLATLY])
 
@@ -33,7 +47,7 @@ preset_dropdown = dcc.Dropdown(
     className="dropdown",
     options=[f"Preset {i}" for i in range(1, 9)],
     value='Choose preset',
-    clearable=True,
+    clearable=False,
     style={"min-width": "8em", "margin-left": "0em", "color": "gray"},
 )
 
@@ -61,15 +75,16 @@ checklist = html.Div(
         html.H3("Prediction models", style={"margin-top": "4.5em"}),
         dbc.Checklist(
             options=[
-                {"label": "Linear regression", "value": "lin_reg", "disabled": False},
-                {"label": "SARIMAX", "value": 1, "disabled": True},
-                {"label": "Prophet", "value": 2, "disabled": True},
-                {"label": "TBATS", "value": 3, "disabled": True},
-                {"label": "Random Forest", "value": 4, "disabled": True},
-                {"label": "XGBoost", "value": 5, "disabled": True},
+                {"label": "Linear regression", "value": "linreg_poly", "disabled": False},
+                {"label": "SARIMAX", "value": "sarimax", "disabled": True},
+                {"label": "Prophet", "value": "prophet", "disabled": True},
+                {"label": "TBATS", "value": "tbats", "disabled": True},
+                {"label": "Random Forest", "value": "random_forest"},
+                {"label": "XGBoost", "value": "xgboost"},
+                {"label": "XGB (reduced features)", "value": "xgboost_reduced_features"},
                 {"label": "Baseline", "value": "baseline"},
             ],
-            value=["lin_reg"],
+            value=["linreg_poly"],
             id="model-selection",
         ),
     ]
@@ -97,7 +112,7 @@ dropdown_time = dcc.Dropdown(
     id="time-selection",
     className="dropdown mx-2",
     options=[str(i).zfill(2) for i in range(24)],
-    value='00',
+    value=set_time,
     #style={'width': '30%', 'display': 'inline-block', 'verticalAlign': 'middle'},
     style={'min-width': '5em'},
     clearable=False,
@@ -110,8 +125,8 @@ datepick = dcc.DatePickerSingle(
     month_format="MMMM, 2022",
     min_date_allowed="2011-01-01",
     max_date_allowed="2011-12-31",
-    initial_visible_month="2011-08-01",
-    date="2011-05-05",
+    initial_visible_month=set_date,
+    date=set_date,
     className="mr-4",
 )
 
@@ -185,6 +200,18 @@ app.layout = html.Div(
 className="container dash-bootstrap mt-0")
 
 @app.callback(
+        Output("datepicker", "date"),
+        Output("time-selection", "value"),
+        Input("preset-dropdown", "value"),
+    )
+def change_date_and_time(preset):
+    if preset == "Choose preset":
+        return (set_date, set_time)
+    else:
+        i = int(preset[-1])
+    return pred_time_hours[i-1]
+
+@app.callback(
         Output("graph", "figure"),
         Input("building-selection", "value"),
         Input("datepicker", "date"),
@@ -193,41 +220,67 @@ className="container dash-bootstrap mt-0")
         Input("config-toggles", "value"),
     )
 def display_graph(building_nr, selected_date, selected_time, models, config_toggles):
-
-    forecast_df = forecasts[building_nr]
-    df = dfs[building_nr]
+    set_date = selected_date
+    set_time = selected_time
     
-    year_start = pd.Timestamp("2011-01-01")
-    year_end = pd.Timestamp("2011-12-31 23:00")
-
-    forecast_df["baseline"] = df.net_load_kW.shift(24*365).loc[year_start:year_end]
+    building_forecasts = all_forecasts[building_nr]
+    
+    year_start = building_forecasts.index[0]
+    year_end = building_forecasts.index[-1]
+    assert year_start == pd.Timestamp("2011-01-01")
+    assert year_end == pd.Timestamp("2011-12-31 23:00")
 
     start_date = pd.Timestamp(f"{selected_date} {selected_time}:00")
+    #current_date = start_date - pd.Timedelta(hours=1)
     day_before = start_date - pd.Timedelta(hours=24)
     end_date = start_date + pd.Timedelta(hours=24)
-    masked_forecasts = forecast_df.loc[start_date+pd.Timedelta(hours=1):end_date]
-    mf2 = forecast_df.loc[day_before:start_date]
+    masked_forecasts = building_forecasts.loc[start_date+pd.Timedelta(hours=1):end_date]
+    mf2 = building_forecasts.loc[day_before:start_date]
 
-    #fig = px.line(masked_forecasts, x="datetime", y="linreg_poly", template="simple_white")
-    fig = px.line(x=masked_forecasts["datetime"], y=[np.nan for _ in masked_forecasts["datetime"]], template="simple_white")
-    if "lin_reg" in models:
-        fig.append_trace({'x':masked_forecasts["datetime"], 'y':masked_forecasts["linreg_poly"]}, 1, 1)
+    fig = px.line(x=masked_forecasts.index, y=[np.nan for _ in masked_forecasts.index], template="simple_white")
 
-    if ("actual" in config_toggles) or ("previousday" in config_toggles):
-        if ("actual" in config_toggles) and ("previousday" in config_toggles):
-            df_actual = forecast_df.loc[day_before:end_date]
-        elif ("actual" in config_toggles):
-            df_actual = forecast_df.loc[start_date:end_date]
-        elif ("previousday" in config_toggles):
-            df_actual = forecast_df.loc[day_before:start_date]
-        fig.append_trace({'x':df_actual["datetime"], 'y':df_actual["net_load_kW"]}, 1, 1)
+    actual_usage = building_forecasts["actual_usage"]
+    current_actual_usage = actual_usage[start_date]
+    if "previousday" in config_toggles:
+        #plotting actual usage in time before
+        df_actual = actual_usage.loc[day_before:start_date]
+        fig.append_trace(
+            {'type': 'scatter',
+             'mode': 'lines',
+             'x': df_actual.index,
+             'y': df_actual,
+             'line': {
+                'color': 'black',
+             }},
+             row=1, col=1,
+        )
+
+    if "actual" in config_toggles:
+        df_actual = actual_usage.loc[start_date:end_date]
+        fig.append_trace(
+            {'type': 'scatter',
+             'mode': 'lines',
+             'x': df_actual.index,
+             'y': df_actual,
+             'line': {
+                'color': 'black',
+                'dash': 'dash',
+             }},
+             row=1, col=1,
+        )
+
+    for model in models:
+        times = [start_date, *masked_forecasts.index]
+        values = [current_actual_usage, *masked_forecasts[model]]
+        #fig.append_trace({'x':masked_forecasts.index, 'y':masked_forecasts[model]}, 1, 1)
+        fig.append_trace({'x':times, 'y':values}, 1, 1)
     
     #TODO: if "previousday" not in config_toggles, deactivate "weather" switch?
     if ("weather" in config_toggles) and ("previousday" in config_toggles):
-        fig.append_trace({'x':mf2["datetime"], 'y':mf2["outdoor_temp"]}, 1, 1)
+        fig.append_trace({'x':mf2.index, 'y':mf2["outdoor_temp"]}, 1, 1)
 
-    if "baseline" in models:
-        fig.append_trace({'x':masked_forecasts["datetime"], 'y':masked_forecasts["baseline"]}, 1, 1)
+    #if "baseline" in models:
+    #    fig.append_trace({'x':masked_forecasts.index, 'y':masked_forecasts.baseline}, 1, 1)
     
     fig.add_vline(x=start_date, line_width=3, line_dash="dash", line_color="green")
     fig.update_layout(showlegend=False,
@@ -243,8 +296,8 @@ def display_graph(building_nr, selected_date, selected_time, models, config_togg
         linecolor="white",
     )
 
-    #fig.update_xaxes(
-    #    tickformat="%H:%M\n Aug %d, 2022")
+    fig.update_xaxes(
+        tickformat="%H:%M\n %b %d, 2022")
 
     return fig
 
